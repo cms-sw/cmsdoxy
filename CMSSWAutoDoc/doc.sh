e400ce56835702e89e210b8173f0d53b11fda927
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-09-10 10:36:08 +0200

    simplify doc.sh and cleanup generate_reference_manual

--- CMSSWAutoDoc/doc.sh
+++ CMSSWAutoDoc/doc.sh
@@ -1,25 +1,15 @@
-#!/bin/bash -x
+#!/bin/bash -ex
 
 # get the scram
 shopt -s expand_aliases
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
-# if you get negative (!=0) result from the last command,
-# print the error message and exit
-function checkError(){
-    if [ $(echo $?) -ne 0 ]; then
-        echo "ERROR: $1"
-        exit 1
-    fi
-}
-
-WORK_DIR=$PWD
-echo "workdir: ${WORK_DIR}"
+WORK_DIR=$(/bin/pwd)
 BASE=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
 cd $BASE
-TMP="TMP"
+TMP="${WORK_DIR}/TMP"
+rm -rf $TMP
 mkdir $TMP
-TMP="${PWD}/TMP"
 ARCH=$ARCHITECTURE
 REL=$RELEASE_FORMAT
 
@@ -32,59 +22,30 @@ exec 2>&1
 # set the architecture
 export SCRAM_ARCH=$ARCH
 cd $TMP
-echo "TMP" $TMP
-# delete old release direcroy in case of an unsuccessful doc process if exists
-if [ -d "$TMP/$REL" ]; then
-    echo "## I saw that the $REL is already there! I will delete it now."
-    rm -rf $TMP/$REL
-fi
 
 # create the CMSSW base
-cmsrel $REL # huh?
-echo $PWD
-#scramv1 project CMSSW $REL
-checkError "CMSSW could not be initialized properly."
+scram project CMSSW $REL
 cd $REL
 
 # set the cms env
-cmsenv # huh?
-#eval `scramv1 runtime -sh`
-checkError "CMSSW environments could not be set."
+eval `scramv1 runtime -sh`
 
 # clone CMSSW repo
-git clone git@github.com:cms-sw/cmssw.git src
-checkError "Repo could not be cloned."
-cd $TMP/$REL/src/
-git checkout $REL
-cd $TMP/$REL
+wget -q https://github.com/cms-sw/cmssw/archive/refs/tags/${REL}.tar.gz
+tar -xzf ${REL}.tar.gz
+rm -rf src ${REL}.tar.gz
+mv cmssw-${REL} src
 
 ######## HARD CODED DOCKIT SECTION ########
-cd $TMP/$REL/src/Documentation/
-echo "go to: $TMP/$REL/src/Documentation/"
-rm -rf ReferenceManualScripts/
-echo "delete: ReferenceManualScripts"
-cp -r $WORK_DIR/ReferenceManualScripts/ .
-checkError "I coldn't copy the directory, i cmsdoxy/cmsdoxy/ReferenceManualScripts/scripts"
-echo "cp -r cmsdoxy/cmsdoxy/ReferenceManualScripts/scripts `pwd`"
-cd $TMP/$REL/src/Documentation/ReferenceManualScripts/scripts 
-tcsh generate_reference_manual > /dev/null
-checkError "I couldn't generate the refman..."
-# add check point here!
+rm -rf src/Documentation/ReferenceManualScripts/
+cp -r $WORK_DIR/ReferenceManualScripts/ src/Documentation/
+pushd src/Documentation/ReferenceManualScripts/scripts 
+  tcsh generate_reference_manual > /dev/null
+popd
 ######## HARD CODED DOCKIT SECTION ########
 
 # clean up the base
-cd $TMP/$REL
 rm -rf biglib/ bin/ cfipython/ config/ include/ lib/ logs/ objs/ python/ src/ test/ tmp/ DocKit/
-#gzip -r -S gz doc/
-echo "generated on $(date)" > auto.doc.2
 
-cd $BASE
-#python $BASE/semaphore.py $IOFILE $REL "documented"
-checkError "$IOFILE could not be updated."
-#scp $IOFILE cmsdoxy@cmssdt01.cern.ch:/data/doxygen/CMSSWAutoDoc
-#cp $IOFILE /eos/project/c/cmsweb/www/cmssdt/doxygen/CMSSWAutoDoc 
-
-# upload ref man files (hardcoded username & machine!)
-echo "## uploading files..."
 cd $TMP
 zip -r ${REL}.zip $REL
